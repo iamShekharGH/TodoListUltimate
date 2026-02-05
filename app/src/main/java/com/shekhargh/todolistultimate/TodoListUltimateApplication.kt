4c23ad92d3754e37174b65706a32936b78b58ec3
shekharhandigol <shekhar1990@gmail.com>
2026-02-05 07:03:26 +0530

    feat: Configure Hilt for dependency injection
    This commit integrates Hilt for dependency injection into the application.
    
    ### Changes:
    - **Application Class**: A new `TodoListUltimateApplication` class was created and annotated with `@HiltAndroidApp` to enable Hilt's code generation.
    - **Android Manifest**: The manifest was updated to reference the new `TodoListUltimateApplication` class.
    - **Main Activity**: The `MainActivity` is now annotated with `@AndroidEntryPoint` to allow for dependency injection within the activity.

---
+++ app/src/main/java/com/shekhargh/todolistultimate/TodoListUltimateApplication.kt
@@ -0,0 +1,8 @@
+package com.shekhargh.todolistultimate
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoListUltimateApplication : Application() {
+}
