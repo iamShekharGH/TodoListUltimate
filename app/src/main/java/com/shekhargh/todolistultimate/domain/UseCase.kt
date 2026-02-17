5a96a28b4786f46ac278d0aa16be5898cea9372a
shekharhandigol <shekhar1990@gmail.com>
2026-02-18 00:09:36 +0530

    feat(domain): Add new use case interfaces
    Add new interfaces for use cases that require multiple inputs or no
    parameters. This enhances the domain layer's flexibility by supporting
    more varied operational requirements.

--- app/src/main/java/com/shekhargh/todolistultimate/domain/UseCase.kt
+++ app/src/main/java/com/shekhargh/todolistultimate/domain/UseCase.kt
@@ -4,9 +4,14 @@ interface SuspendUseCase<in I, out O> {
     suspend operator fun invoke(input: I): O
 }
 
+interface SuspendUseCaseMultipleInput<in I, in I2, out O> {
+    suspend operator fun invoke(input: I, input2: I2): O
+}
+
 interface UseCase<in I, out O> {
     operator fun invoke(param: I): O
 }
+
 interface NoParameterUseCase<out O> {
     operator fun invoke(): O
 }
